4f00f6094c0ea53b0044addf6f4e73611180ec5d
GuillaumeAssier <sykursen@protonmail.com>
2026-04-01 16:45:42 +0200

    fix: Firefox extensions

--- modules/features/browser.nix
+++ modules/features/browser.nix
@@ -20,11 +20,11 @@
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
             installation_mode = "force_installed";
           };
-          "{446900e4-71c2-419f-a6d7-af074f4f0c7e}" = {
+          "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
             installation_mode = "force_installed";
           };
-          "{c26f2bf6-0667-411c-9d0f-a2a7a5a1a60e}" = {
+          "emoji@saveriomorelli.com" = {
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/emoji-sav/latest.xpi";
             installation_mode = "force_installed";
           };
